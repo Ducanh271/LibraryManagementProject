440ba154748edfb888b2a13c62058150a69edcc5
Duc Anh <nguyenduca03@gmail.com>
2024-11-01 15:24:54 +0700

    test

---
+++ Test0/src/test0/Test0.java
@@ -0,0 +1,14 @@
+package test0;
+public class Test0 {
+
+    public static void main(String[] args) {
+       Login LoginFrame = new Login();
+       LoginFrame.setVisible(true);
+       LoginFrame.pack();
+       LoginFrame.setLocationRelativeTo(null);
+       
+       
+       
+    }
+}
+
