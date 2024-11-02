d0c634413bf210c806dd61710fcaf65ae5ccd10f
Duc Anh <nguyenduca03@gmail.com>
2024-11-02 22:51:47 +0700

    ban chuan

---
+++ 3.LibManagementApp/src/common/view/Test0.java
@@ -0,0 +1,14 @@
+package common.view;
+public class Test0 {
+
+    public static void main(String[] args) {
+       Login LoginFrame = new Login();
+       LoginFrame.setVisible(true);
+       LoginFrame.pack();
+       LoginFrame.setLocationRelativeTo(null);
+       
+       
+       
+    }
+}
+
